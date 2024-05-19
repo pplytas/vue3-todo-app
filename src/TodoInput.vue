3be0ce696389e72f55af922fe685bea58dbc1a94
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2024-05-19 21:24:15 +0300

    Re-focus input after adding todo

--- src/TodoInput.vue
+++ src/TodoInput.vue
@@ -4,16 +4,19 @@ import { ref } from 'vue'
 const emit = defineEmits<{ addTodo: [newTodo: string] }>()
 
 const newTodo = ref('')
+const input = ref<HTMLInputElement>()
 
 const addTodo = () => {
   emit('addTodo', newTodo.value)
   newTodo.value = '' // Reset the input
+  input.value?.focus() // Focus the input
 }
 </script>
 
 <template>
   <form class="space-x-2" @submit.prevent="addTodo">
     <input
+      ref="input"
       class="border border-slate-900 h-[40px] px-3 rounded-md shadow"
       type="text"
       v-model="newTodo"
